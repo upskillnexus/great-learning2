c9095b6a9f6f61f56a207ea24e66a292ecd3f5a2
Arun Kumar ( Katill_Boyy ) <arun07744@gmail.com>
2023-12-30 16:52:37 +0530

    123

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,12 @@ import { MainRoutes } from "./Routes/MainRoutes";
 function App() {
   return (
     <>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="UJZrynxLRf98jL1vgmRluBLHhO2Rvd3kNXu5ijdMKuk"
+        />
+      </head>
       <MainRoutes />
     </>
   );
